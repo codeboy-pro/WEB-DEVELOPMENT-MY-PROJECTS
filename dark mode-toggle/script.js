1d149f27ab645072431fce28cf1096942f3e0b98
Pradip Maity <pradipmaity9907@gmail.com>
2025-04-05 20:59:52 +0530

    Add files via upload

---
+++ dark mode-toggle/script.js
@@ -0,0 +1,28 @@
+const inputEl=document.
+querySelector(".input");
+const bodyEl=document.querySelector("body");
+// console.log(inputEl.checked);
+inputEl.checked=JSON.parse(localStorage.
+getItem("mode"));
+update();
+
+function update(){
+    if(inputEl.checked){
+        bodyEl.style.background="black";
+    }
+    else{
+        bodyEl.style.background="white";
+    }
+
+}
+
+inputEl.addEventListener("input",()=>{
+    update();
+    updateLocalStorage();
+})
+
+function updateLocalStorage(){
+    localStorage.setItem("mode",
+        JSON.stringify(inputEl.checked)
+    );
+}
